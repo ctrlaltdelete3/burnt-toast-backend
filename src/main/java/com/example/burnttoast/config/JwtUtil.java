611e1c938ec855465b04bbb5caf393501a62c9a3
ctrlaltdelete3 <ivana.bevanda1@gmail.com>
2026-04-20 11:56:48 +0200

    change - using fixed JWT secret from environment variable

--- src/main/java/com/example/burnttoast/config/JwtUtil.java
+++ src/main/java/com/example/burnttoast/config/JwtUtil.java
@@ -1,8 +1,9 @@
 package com.example.burnttoast.config;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -10,21 +11,27 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private final Key secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    @Value("${jwt.secret}")
+    private String secret;
+
     private final long EXPIRATION = 1000 * 60 * 60 * 24; // 24 hours
+
+    private Key getSecretKey(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+    }
 
     public String generateToken(String username){
         return Jwts.builder()
                 .setSubject(username)
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
                 .setIssuedAt(new Date())
-                .signWith(secretKey)
+                .signWith(getSecretKey())
                 .compact();
     }
 
     public String extractUsername(String token){
         return Jwts.parserBuilder()
-                .setSigningKey(secretKey)
+                .setSigningKey(getSecretKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody()
