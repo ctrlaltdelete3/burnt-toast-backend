789c301b2ed9998493d9eca836f7660cc58d839e
ctrlaltdelete3 <ivana.bevanda1@gmail.com>
2026-04-21 11:39:38 +0200

    restrict CORS to Netlify producion URL

--- src/main/java/com/example/burnttoast/config/SecurityConfig.java
+++ src/main/java/com/example/burnttoast/config/SecurityConfig.java
@@ -42,10 +42,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("https://sparkling-toffee-a45431.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(false);
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
